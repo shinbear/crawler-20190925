c84f0310dedd7cf27ef73f0ed724a026f6317e45
shinbear <shinbear@DESKTOP-9M73S2V>
2020-01-16 10:46:41 +0800

    2020-0116-v2

--- crawler/src/main/java/com/waverly/crawler/Main48_W.java
+++ crawler/src/main/java/com/waverly/crawler/Main48_W.java
@@ -401,6 +401,7 @@ public class Main48_W {
 			((ChromeDriver) webDriver).findElementByXPath("//*[@id='btnSearch']").click();
 			return 1;
 		} catch (Exception e2) {
+			writrintExcel();
 			System.out.print(e2);
 			return 0;
 		}
